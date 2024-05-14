407757a7554a7b0106ccdef0bbcc600724b21e29
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2024-05-14 10:59:06 +0300

    Initial commit

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,22 @@
+function accordion () {
+    const btns = document.querySelectorAll('.mobile-links__parent-btn');
+
+    btns.forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            const content = btn.nextElementSibling;
+            
+            const isOpen = btn.classList.toggle('active');
+
+            if (isOpen) {
+                // элемент будет раскрыт на  необходимую ему высоту
+                content.style.maxHeight = content.scrollHeight + 'px';            
+            } else {
+                content.style.maxHeight = '0px';
+            }
+            
+            
+        })
+    })
+}
+
+export default accordion;
